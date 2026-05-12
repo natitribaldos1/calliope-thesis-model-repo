c339abb7cafb90bbb32966a808a4cd3813fe91f8
natitribaldos1 <natitribaldos@gmail.com>
2026-05-12 16:44:27 +0200

    update run_model file

--- run_model.bat
+++ run_model.bat
@@ -1,9 +1,28 @@
 @echo off
 
-CALL "%USERPROFILE%\anaconda3\Scripts\activate.bat"
-CALL conda activate calliope-thesis-env
+IF EXIST "%USERPROFILE%\anaconda3\Scripts\activate.bat" (
+    CALL "%USERPROFILE%\anaconda3\Scripts\activate.bat"
+    CALL conda activate calliope-thesis-env
+
+) ELSE IF EXIST "%USERPROFILE%\anaconda3\condabin\conda.bat" (
+    CALL "%USERPROFILE%\anaconda3\condabin\conda.bat" activate calliope-thesis-env
+
+) ELSE IF EXIST "%USERPROFILE%\AppData\Local\anaconda3\condabin\conda.bat" (
+    CALL "%USERPROFILE%\AppData\Local\anaconda3\condabin\conda.bat" activate calliope-thesis-env
+
+) ELSE IF EXIST "%USERPROFILE%\AppData\Local\anaconda3\Library\bin\conda.bat" (
+    CALL "%USERPROFILE%\AppData\Local\anaconda3\Library\bin\conda.bat" activate calliope-thesis-env
+
+) ELSE (
+    echo Could not find Anaconda/Conda activation file.
+    echo Checked common user-level Anaconda paths.
+    pause
+    exit /b 1
+)
 
 echo Saving results in outputs folder
+
+IF NOT EXIST outputs mkdir outputs
 
 echo Running without_transformer scenario...
 calliope run model.yaml --scenario=without_transformer --save_netcdf="outputs\without_transformer.nc"
